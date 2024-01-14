95891e649e4722a09d5f39e2303134b80560d9bf
tgaytan <tgaytan14@utexas.edu>
2024-01-14 14:33:26 -0600

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Footer from './components/Footer'
 function App() {
   return (
     <>
+      <div className="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <button className="btn btn-primary">Primary button</button>
+      </div>
       <Header />
       <Footer />
     </>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,19 @@ function Footer() {
     return (
         <>
             <h5>this is an h5 footer</h5>
+            <div className="container text-center">
+                <div className="row align-items-start">
+                    <div className="col">
+                    One of three columns
+                    </div>
+                    <div className="col">
+                    One of three columns
+                    </div>
+                    <div className="col">
+                    One of three columns
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
