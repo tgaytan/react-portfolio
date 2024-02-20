3438d7e2ab0b299f407748960b3b9065cd7ea7e0
tgaytan <tgaytan14@utexas.edu>
2024-02-19 19:15:28 -0600

    added subtitle to portfolio page and styled it

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,15 +3,18 @@ import projects from '../projects'
 
 function Portfolio() {
     return (
-        <div id="portfolio-body">
-                {projects.map(project => {
-                    return (
-                        <div key={project.name} className="project-pic">
-                            <Project projects={project} key={project.name}/>
-                        </div>
-                    )
-                })}    
-        </div>
+        <>
+            <h1 className="text-background title" id="portfolio-title">Portfolio</h1>
+            <div id="projects-body">
+                    {projects.map(project => {
+                        return (
+                            <div key={project.name} className="project-pic">
+                                <Project projects={project} key={project.name}/>
+                            </div>
+                        )
+                    })}    
+            </div>
+        </>
     )
 }
 
