16f8acf0bbea44f55a6009771abf41ba9b1a682c
tgaytan <tgaytan14@utexas.edu>
2024-02-20 20:08:52 -0600

    updated Resume page. added skills. need to work on making the resume easy to download

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,7 +1,35 @@
 function Resume() {
     return (
         <>
-            <p>Resume Page</p>
+            <div className="page-body">
+                <h1 className="text-background title">Resume</h1>
+                <div className="text-background text">
+                    <h2>Download my resume</h2>
+                </div>
+                <div className="text-background text">
+                    <h3>Skills</h3>
+                    <ul>
+                        <li>React</li>
+                        <li>NoSQL</li>
+                        <li>Node</li>
+                        <li>Express</li>
+                        <li>REST API</li>
+                        <li>GraphQL</li>
+                        <li>Git</li>
+                        <li>Sequelize</li>
+                        <li>Application Troubleshooting</li>
+                        <li>Active Directory</li>
+                        <li>PostgreSQL, MSSQL, Oracle, MySQL</li>
+                        <li>Linux</li>
+                        <li>SSL</li>
+                        <li>Log Analysis</li>
+                        <li>Wireshark</li>
+                        <li>High Availability</li>
+                        <li>Knowledge Capture</li>
+                        <li>Amazon S3</li>
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
