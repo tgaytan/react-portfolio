fb53c1ab3f1a6a7cfb0399b7713e340456882336
tgaytan <tgaytan14@utexas.edu>
2024-02-22 18:10:29 -0600

    removed bootstrap classes from nav bar. needs more styling to match page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,21 +5,11 @@ function Nav() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link to="/" className={currentPage === '/' ? 'nav-link active' : 'nav-link'} aria-current="page" >About</Link>
-                        <Link to="/Portfolio" className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Portfolio</Link>
-                        <Link to="/Contact" className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Contact</Link>
-                        <Link to="/Resume" className={currentPage === '/' ? 'nav-link active' : 'nav-link'} >Resume</Link>
-                    </div>
-                    </div>
-                </div>
+            <nav>
+                <Link to="/" className="active-link" aria-current="page">About</Link>
+                <Link to="/Portfolio">Portfolio</Link>
+                <Link to="/Contact">Contact</Link>
+                <Link to="/Resume">Resume</Link>
             </nav>
         </>
     )
